ba18a63d784d69c8f0f9f01cf6ee098879d03504
Kristin Gadgil <kristingadgil@gmail.com>
2021-10-05 12:07:58 -0500

    Reconfigured sql file for deployment

--- db/schema.sql
+++ db/schema.sql
@@ -16,6 +16,9 @@ CREATE TABLE questions (
   date_written TIMESTAMP NULL DEFAULT NULL
 );
 
+\COPY questions (question_id, product_id, body, epoch, asker_name, asker_email, reported, helpful) FROM '/Users/kristingadgil/Repos/questions-answers/csv_original/questions.csv' DELIMITER ',' CSV HEADER;
+
+
 DROP TABLE IF EXISTS answers;
 
 CREATE TABLE answers (
@@ -33,6 +36,9 @@ CREATE TABLE answers (
       REFERENCES questions(question_id)
 );
 
+\COPY answers (answer_id, question_id, body, epoch, answerer_name, answerer_email, reported, helpful) FROM '/Users/kristingadgil/Repos/questions-answers/csv_original/answers.csv' DELIMITER ',' CSV HEADER;
+
+
 DROP TABLE IF EXISTS answers_photos;
 
 CREATE TABLE answers_photos (
@@ -43,3 +49,27 @@ CREATE TABLE answers_photos (
     FOREIGN KEY(answer_id)
       REFERENCES answers(answer_id)
 );
+
+\COPY answers_photos (photo_id, answer_id, photo_url) FROM '/Users/kristingadgil/Repos/questions-answers/csv_original/answers_photos.csv' DELIMITER ',' CSV HEADER;
+
+
+
+UPDATE questions SET date_written = to_timestamp(floor(epoch/1000));
+
+UPDATE answers SET date_written = to_timestamp(floor(epoch/1000));
+
+ALTER TABLE questions DROP COLUMN epoch;
+
+ALTER TABLE answers DROP COLUMN epoch;
+
+SELECT setval('questions_question_id_seq', max(question_id)) from questions;
+
+SELECT setval('answers_answer_id_seq', max(answer_id)) from answers;
+
+SELECT setval('answers_photos_photo_id_seq', max(photo_id)) from answers_photos;
+
+CREATE INDEX ON questions (product_id);
+
+CREATE INDEX ON answers (question_id);
+
+CREATE INDEX ON answers_photos (answer_id);
