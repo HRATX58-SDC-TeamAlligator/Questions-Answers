62bf3de5642bf05d837c747785f456feadb9b136
Kristin Gadgil <kristingadgil@gmail.com>
2021-10-07 12:26:31 -0500

    Added Redis and re-factored code

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const queries = require('../db/queries.js');
+const redis = require('redis');
 const PORT = 3004;
 const app = express();
 
 // const loader = require('./loaderio-bbb7b646b318c928672c6087937a878b.txt');
 // require('newrelic');
 
-
+const client = redis.createClient();
 app.use(cors());
 app.use(compression());
 
@@ -19,28 +20,75 @@ app.use(compression());
 //     res.send(loader);
 //   }
 // });
-
-app.get(`/qa/:id`, (req, res) => {
-  const product_id = req.params.id;
-  queries.getQAs(product_id, (err, response) => {
-  if (err) {
-    res.status(400).send('Questions request unsuccessful');
-  } else {
-    res.send(response.rows);
-  }
-  });
+client.on('connect', ()=> {
+  console.info('Redis connection established');
 })
+
+app.get('/qa/:id', (req, res) => {
+  const product_id = req.params.id;
+  try {
+    client.get(`questions:${product_id}`, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      if (data) {
+        console.log('questions retrieved from cache');
+        res.send(JSON.parse(data));
+      } else {
+        console.log('questions retrieved from db');
+        queries.getQAs(product_id, (err, response) => {
+          if (err) {
+            res.status(400).send('questions request unsuccessful');
+          } else {
+            res.send(response.rows);
+            client.set(
+              `questions:${product_id}`,
+              JSON.stringify(response.rows),
+              'EX',
+              //2 days
+              172800
+            );
+          }
+        });
+      }
+    });
+  } catch (err) {
+    res.status(400).send('did not retrieve questions');
+  }
+});
 
 app.get(`/qa/questions/:id/answers`, (req, res) => {
   const question_id = req.params.id;
-  queries.getSpecificAnswers(question_id, (err, response) => {
-    if (err) {
-      res.status(400).send('Answers request unsuccessful');
-    } else {
-      res.send(response.rows);
-    }
-  });
-})
+  try {
+    client.get(`answers:${question_id}`, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      if (data) {
+        console.log('answers retrieved from cache');
+        res.send(JSON.parse(data));
+      } else {
+        console.log('answers retrieved from db');
+        queries.getSpecificAnswers(question_id, (err, response) => {
+          if (err) {
+            res.status(400).send('answers request unsuccessful')
+          } else {
+            res.send(response.rows);
+            client.set(
+              `answers:${question_id}`,
+              JSON.stringify(response.rows),
+              'EX',
+              172800
+            );
+          }
+        });
+      }
+    });
+  } catch (err) {
+    res.status(400).send('did not retrieve answers')
+  }
+});
+
 
 app.post(`/qa/:id`, (req, res) => {
   const product_id = req.params.id;
@@ -49,9 +97,10 @@ app.post(`/qa/:id`, (req, res) => {
   const asker_email = req.query.email;
   queries.askQuestion(product_id, body, asker_name, asker_email, (err, response) => {
     if (err) {
-      res.status(400).send('Question post unsuccessful');
+      res.status(400).send('question post unsuccessful');
     } else {
-      res.status(201).send('Question posted');
+      client.del(JSON.stringify(product_id));
+      res.status(201).send('question posted');
     }
   });
 });
@@ -66,6 +115,7 @@ app.post(`/qa/answers/:id`, (req, res) => {
     if (err) {
       res.status(400).send('Answer post unsuccessful');
     } else {
+      client.del(JSON.stringify(question_id));
       if (!photos.length) {
         res.status(201).send('Answer posted');
       } else {
